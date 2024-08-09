09cdd20f2aaf3809c386ac18f6636e21263eb946
saad <102238602+simitry@users.noreply.github.com>
2024-08-09 19:04:36 +0200

    progress

--- food_delivery/my_react_app/src/Components/FoodItem/FoodItem.jsx
+++ food_delivery/my_react_app/src/Components/FoodItem/FoodItem.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 
-const FoodItem = ({id,name,price,description,image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
 
-  return (
-    <div className="food-item">
-        <div className="food-item-img-container">
-            <img className="food-item-image" src={image} alt="" />
-        </div>
-        <div className="food-item-name-rating">
-            <p>{name}</p>
-            <img src={assets.rating_starts} alt="" />
+    const [itemCount,setItemCount] = useState(0);
 
+    return (
+        <div className="food-item">
+            <div className="food-item-img-container">
+                <img className="food-item-image" src={image} alt="" />
+                {!itemCount
+                    ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white}/>
+                    :<div className='food-item-counter'>
+                        <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt=''/>
+                        <p>{itemCount}</p>
+                        <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+                    </div>
+
+                }
+            </div>
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p>{name}</p>
+                    <img src={assets.rating_starts} alt="" />
+                </div>
+                <p className="food-item-desc">{description}</p>
+                <p className="food-item-price">{price+50} MAD</p>
+            </div>
         </div>
-        <p className="food-item-desc">{description}</p>
-        <p className="food-item-price">{price} MAD</p>
-    </div>
-  )
+    )
 }
 
-export default FoodItem
+export default FoodItem;
